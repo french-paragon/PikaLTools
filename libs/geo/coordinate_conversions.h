2239d089a27ce842bdc074e18fccd0e0c605e7f0
french-paragon <french.paragon@gmail.com>
2023-04-24 17:44:39 +0200

    Add a tool to inspect the LCF file.

---
+++ libs/geo/coordinate_conversions.h
@@ -0,0 +1,133 @@
+#ifndef COORDINATE_CONVERSIONS_H
+#define COORDINATE_CONVERSIONS_H
+
+#include <cmath>
+#include <vector>
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+struct EllipsoidDefinition {
+    double equatorialRadius;
+    double polarRadius;
+};
+
+constexpr EllipsoidDefinition WGS84_Ellipsoid = {6378137.0, 6356752.3142};
+
+template<typename T>
+struct CartesianCoord {
+    T x;
+    T y;
+    T z;
+};
+
+template<typename CT>
+inline CartesianCoord<CT> convertLatLonToECEF(CT lat, CT lon, CT alt, EllipsoidDefinition ellipsoid) {
+
+    CT latRad = lat*M_PI/180;
+    CT lonRad = lon*M_PI/180;
+
+    CT x_lat;
+    CT z_lat;
+    CT radius_lat;
+
+    if (std::tan(std::fabs(latRad)) < std::tan(M_PI_2 - std::fabs(latRad))) {
+
+        CT equatorial = 1/ellipsoid.equatorialRadius;
+        CT polar = std::tan(std::fabs(latRad))/ellipsoid.polarRadius;
+
+        x_lat = std::sqrt(1/((equatorial*equatorial + polar*polar)));
+
+        CT tmp = 1 - x_lat*x_lat*equatorial*equatorial;
+
+        radius_lat = std::sqrt(ellipsoid.polarRadius*ellipsoid.polarRadius*tmp + 1/((equatorial*equatorial + polar*polar)));
+
+        if (tmp <= 0) {
+            z_lat = 0;
+        } else {
+
+            z_lat = ellipsoid.polarRadius*std::sqrt(tmp);
+
+            if (lat < 0) {
+                z_lat = -z_lat;
+            }
+        }
+    } else {
+
+        CT equatorial = std::tan(M_PI_2 - std::fabs(latRad))/ellipsoid.equatorialRadius;
+        CT polar = 1/ellipsoid.polarRadius;
+
+        z_lat = std::sqrt(1/((equatorial*equatorial + polar*polar)));
+
+        if (lat < 0) {
+            z_lat = -z_lat;
+        }
+
+        CT tmp = 1 - z_lat*z_lat*polar*polar;
+
+        radius_lat = std::sqrt(ellipsoid.equatorialRadius*ellipsoid.equatorialRadius*tmp + 1/((equatorial*equatorial + polar*polar)));
+
+        if (tmp <= 0) {
+            x_lat = 0;
+        } else {
+
+            x_lat = ellipsoid.equatorialRadius*std::sqrt(tmp);
+        }
+
+    }
+
+    CT scaling = (radius_lat+alt)/radius_lat;
+    x_lat *= scaling;
+    z_lat *= scaling;
+
+    CartesianCoord<CT> ret;
+    ret.x = std::cos(lonRad)*x_lat;
+    ret.y = std::sin(lonRad)*x_lat;
+    ret.z = z_lat;
+
+    return ret;
+
+}
+
+/*!
+ * \brief getLocalFrameAtPos get the affine transform from ECEF to a local frame at the position on the ellipsoid
+ * \param lat the current latitude
+ * \param lon the current longitude
+ * \param ellipsoid the ellipsoid to use
+ * \return a 3x4 matrix, representing the affine transform from ECEF to the local frame.
+ */
+template<typename CT>
+inline Eigen::Matrix<CT,3,4> getLocalFrameAtPos(CT lat, CT lon, EllipsoidDefinition ellipsoid) {
+
+    CT latRad = lat*M_PI/180;
+    CT lonRad = lon*M_PI/180;
+
+    CartesianCoord<CT> zero = convertLatLonToECEF<CT>(lat, lon, 0, ellipsoid);
+    CartesianCoord<CT> z_one = convertLatLonToECEF<CT>(lat, lon, 1, ellipsoid);
+
+    Eigen::Matrix<CT,3,1> vec_zero(zero.x, zero.y, zero.z);
+    Eigen::Matrix<CT,3,1> vec_z_one(z_one.x, z_one.y, z_one.z);
+
+    Eigen::Matrix<CT,3,1> x_local(-std::sin(lonRad), std::cos(lonRad), 0);
+    Eigen::Matrix<CT,3,1> z_local = vec_z_one - vec_zero;
+    Eigen::Matrix<CT,3,1> y_local = z_local.cross(x_local);
+
+
+    Eigen::Matrix<CT,3,1> vec_x_one = vec_zero + x_local;
+    Eigen::Matrix<CT,3,1> vec_y_one = vec_zero + y_local;
+
+    Eigen::Matrix<CT,3,3> Rlocal2ecef;
+    Rlocal2ecef.template block<3,1>(0,0) = x_local;
+    Rlocal2ecef.template block<3,1>(0,1) = y_local;
+    Rlocal2ecef.template block<3,1>(0,2) = z_local;
+
+    Eigen::Matrix<CT,3,3> Recef2local = Rlocal2ecef.transpose();
+
+    Eigen::Matrix<CT,3,4> ret;
+    ret.template block<3,3>(0,0) = Recef2local;
+    ret.template block<3,1>(0,3) = -Recef2local*vec_zero;
+
+    return ret;
+}
+
+#endif // COORDINATE_CONVERSIONS_H
---
+++ tools/lcf_file_viewer/main.cpp
@@ -0,0 +1,218 @@
+#include "libs/io/read_envi_bil.h"
+#include "libs/geo/coordinate_conversions.h"
+
+#include <qImageDisplayWidget/imagewindow.h>
+#include <qImageDisplayWidget/imageadapter.h>
+
+#include <QApplication>
+#include <QTextStream>
+#include <QVector>
+
+#include <LibStevi/utils/types_manipulations.h>
+
+#define QCUSTOMPLOT_USE_LIBRARY
+#include "qcustomplot.h"
+
+struct timedPositions {
+    CartesianCoord<double> pos;
+    double time;
+};
+
+QVector<timedPositions> getINSMotionValue(QVector<QString> const& files) {
+
+    QVector<timedPositions> accumulated;
+
+    double meanLat = 0;
+    double meanLon = 0;
+
+    for (QString const& file : files) {
+        std::vector<EnviBilLcfLine> lines = read_envi_bil_lcf_data(file.toStdString());
+
+        accumulated.reserve(accumulated.size() + lines.size());
+
+        for (EnviBilLcfLine const& line : lines) {
+
+            double lat= line.lat;
+            double lon = line.lon;
+            double alt = line.height;
+
+            meanLat += lat;
+            meanLon += lon;
+
+            accumulated.push_back({convertLatLonToECEF<double>(lat, lon, alt, WGS84_Ellipsoid), line.timeStamp});
+        }
+    }
+
+    meanLat /= accumulated.size();
+    meanLon /= accumulated.size();
+
+    Eigen::Matrix<double,3,4> ecef2local = getLocalFrameAtPos<double>(meanLat, meanLon, WGS84_Ellipsoid);
+
+    for (timedPositions & pos : accumulated) {
+
+        Eigen::Matrix<double,4,1> homogeneous;
+        homogeneous[0] = pos.pos.x;
+        homogeneous[1] = pos.pos.y;
+        homogeneous[2] = pos.pos.z;
+        homogeneous[3] = 1;
+
+        Eigen::Matrix<double,3,1> local = ecef2local*homogeneous;
+        pos.pos.x = local[0];
+        pos.pos.y = local[1];
+        pos.pos.z = local[2];
+    }
+
+    return accumulated;
+}
+
+int main(int argc, char** argv) {
+
+    QTextStream out(stdout);
+
+    QApplication app(argc, argv);
+
+    QVector<QString> filenames(argc-1);
+
+    for (int i = 1; i < argc; i++) {
+        filenames[i-1] = QString(argv[i]);
+    }
+
+    QVector<timedPositions> ins_signal = getINSMotionValue(filenames);
+
+    QVector<double> posx_values(ins_signal.size());
+    QVector<double> posy_values(ins_signal.size());
+    QVector<double> posz_values(ins_signal.size());
+    QVector<double> timestamps(ins_signal.size());
+    QVector<double> frameNumbers(ins_signal.size());
+
+    for (int i = 0; i < ins_signal.size(); i++) {
+
+        frameNumbers[i] = i;
+        posx_values[i] = ins_signal[i].pos.x;
+        posy_values[i] = ins_signal[i].pos.y;
+        posz_values[i] = ins_signal[i].pos.z;
+        timestamps[i] = ins_signal[i].time;
+    }
+
+    QCustomPlot tzPlot;
+    tzPlot.resize(960, 600);
+
+    QCPGraph* graph_pos = tzPlot.addGraph();
+
+    graph_pos->setData(frameNumbers, posz_values);
+    graph_pos->setPen(QColor(0,0,255));
+
+    graph_pos->rescaleAxes();
+
+    tzPlot.xAxis->setLabel("t");
+    tzPlot.yAxis->setLabel("z");
+
+    tzPlot.setWindowTitle("Z as a function of time");
+
+    tzPlot.setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+    tzPlot.replot();
+    tzPlot.show();
+
+    QCustomPlot tyPlot;
+    tyPlot.resize(960, 600);
+
+    graph_pos = tyPlot.addGraph();
+
+    graph_pos->setData(frameNumbers, posy_values);
+    graph_pos->setPen(QColor(0,255,0));
+
+    graph_pos->rescaleAxes();
+
+    tyPlot.xAxis->setLabel("t");
+    tyPlot.yAxis->setLabel("y");
+
+    tyPlot.setWindowTitle("Y as a function of time");
+
+    tyPlot.setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+    tyPlot.replot();
+    tyPlot.show();
+
+    QCustomPlot txPlot;
+    txPlot.resize(960, 600);
+
+    graph_pos = txPlot.addGraph();
+
+    graph_pos->setData(frameNumbers, posx_values);
+    graph_pos->setPen(QColor(255,0,0));
+
+    graph_pos->rescaleAxes();
+
+    txPlot.xAxis->setLabel("t");
+    txPlot.yAxis->setLabel("x");
+
+    txPlot.setWindowTitle("X as a function of time");
+
+    txPlot.setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+    txPlot.replot();
+    txPlot.show();
+
+    QCustomPlot xyPlot;
+    xyPlot.resize(960, 600);
+
+    graph_pos = xyPlot.addGraph();
+
+    graph_pos->setData(posx_values, posy_values);
+    graph_pos->setPen(QColor(255,255,0));
+
+    graph_pos->rescaleAxes();
+
+    xyPlot.xAxis->setLabel("x");
+    xyPlot.yAxis->setLabel("y");
+
+    xyPlot.setWindowTitle("XY plane trajectory");
+
+    xyPlot.setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+    xyPlot.replot();
+    xyPlot.show();
+
+    QCustomPlot xzPlot;
+    xzPlot.resize(960, 600);
+
+    graph_pos = xzPlot.addGraph();
+
+    graph_pos->setData(posx_values, posz_values);
+    graph_pos->setPen(QColor(255,0,255));
+
+    graph_pos->rescaleAxes();
+
+    xzPlot.xAxis->setLabel("x");
+    xzPlot.yAxis->setLabel("z");
+
+    xzPlot.setWindowTitle("XZ plane trajectory");
+
+    xzPlot.setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+    xzPlot.replot();
+    xzPlot.show();
+
+    QCustomPlot yzPlot;
+    yzPlot.resize(960, 600);
+
+    graph_pos = yzPlot.addGraph();
+
+    graph_pos->setData(posy_values, posz_values);
+    graph_pos->setPen(QColor(0,255,255));
+
+    graph_pos->rescaleAxes();
+
+    yzPlot.xAxis->setLabel("y");
+    yzPlot.yAxis->setLabel("z");
+
+    yzPlot.setWindowTitle("YZ plane trajectory");
+
+    yzPlot.setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+    yzPlot.replot();
+    yzPlot.show();
+
+    return app.exec();
+}
